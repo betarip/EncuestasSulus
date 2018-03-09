d483809e4b9960f01d95f3c2e55d0401e8898460
betaripv <ejemplo@seppue.gob.mx>
2018-03-09 00:00:18 -0600

    Intento de animacion (mejorar)

---
+++ app/src/main/java/com/sulus/encuestasapp/Vistas/TerminoEncuesta.java
@@ -0,0 +1,48 @@
+package com.sulus.encuestasapp.Vistas;
+
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.sulus.encuestasapp.R;
+
+public class TerminoEncuesta extends AppCompatActivity {
+
+    private ImageView checkView;
+    private ImageView crossView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termino_encuesta);
+
+        checkView = (ImageView) findViewById(R.id.check);
+        //crossView = (ImageView) findViewById(R.id.cross);
+
+        ((Animatable) checkView.getDrawable()).start();
+
+        /*
+        checkView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+*/
+
+    }
+
+    public void regresoPanel(View view) {
+        Intent home = new Intent(this, TerminoEncuesta.class);
+
+        home.addCategory(Intent.CATEGORY_HOME);
+        home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(home);
+    }
+}
